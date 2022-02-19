ea807eecbb12ce7dfa9cb4ccff08518718ad4094
ejjr6 <yej00725@gmail.com>
2022-02-19 23:32:16 +0900

    test

---
+++ mvc_part13/src/test/java/net/koreate/common/DAOTest.java
@@ -0,0 +1,26 @@
+package net.koreate.common;
+
+import javax.inject.Inject;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+import net.koreate.user.dao.UserDAO;
+import net.koreate.user.vo.UserVO;
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@ContextConfiguration(
+		locations= {"classpath:context/root-context.xml"}
+)
+public class DAOTest {
+	@Inject
+	UserDAO dao;
+	
+	@Test
+	public void userDaoTest() throws Exception{
+		UserVO vo = dao.getUserById("id001");
+		System.out.println(vo);
+	}
+}
